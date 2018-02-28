a45bbad5b26a8df00314924f1e0a6b0e042f9bd4
Luan Maia <luan.dias@dcomp.sor.ufscar.br>
2018-02-28 09:46:04 -0300

    Criação do projeto para Rest

---
+++ PlaceRaterAPI/PlaceRaterAPI/Entities/Image.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaceRaterAPI
+{
+    public class Image
+    {
+        [Key]
+        [Column(Order = 1)]
+        public string Url { get; set; }
+        [Key]
+        [Column(Order = 2)]
+        [ForeignKey("Place")]
+        public string Name { get; set; }
+        [Key]
+        [Column(Order = 3)]
+        [ForeignKey("Place")]
+        public string City { get; set; }
+        [Key]
+        [Column(Order = 4)]
+        [ForeignKey("Place")]
+        public string State { get; set; }
+
+        public Place Place { get; set; }
+    }
+}
---
+++ PlaceRaterAPI/PlaceRaterRestAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PlaceRaterRestAPI.WebApiApplication" Language="C#" %>
